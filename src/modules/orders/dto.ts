66678114086870d0ed8edbd36d16e686176c2967
Nikita Machekhin <nicitamac@gmail.com>
2020-10-26 19:45:26 +0200

    Create order validation (API validation) in DTO

--- src/modules/orders/dto.ts
+++ src/modules/orders/dto.ts
@@ -1,6 +1,30 @@
 import { DriverStatuses } from './../../db/enums';
 
 export class Order {
+  @IsInt()
+  @Min(0)
+  clientId: number;
+
+  @IsInt()
+  @Min(0)
+  fromId: number;
+
+  @IsInt()
+  @Min(0)
+  toId: number;
+  constructor(clientId: number, fromId: number, toId: number) {
+    this.clientId = clientId;
+    this.fromId = fromId;
+    this.toId = toId;
+  }
+}
+
+export class CreatedOrder {
+  constructor(public orderId: number, public driverId: number) {}
+}
+
+export class MetaxiError {
+  public error: boolean;
   constructor(
     public clientId: number,
     public fromId: number,
