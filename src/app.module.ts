709a5e076c7ddfdd24567d132e690dcb86cf87de
Nikita Machekhin <nicitamac@gmail.com>
2020-10-26 19:53:54 +0200

    fixup! Implements interaction with repository in orders controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { OrdersModule } from './modules/orders/orders.module';
 import { StatisticsModule } from './modules/statistics/statistics.module';
+import { OrdersRepository } from './db/repositories/order.repository';
 
 @Module({
   imports: [OrdersModule, StatisticsModule],
   controllers: [],
-  providers: [],
+  providers: [OrdersRepository],
 })
 export class AppModule {}
