741b632d1a2539801f3a351d8be0192d59732596
Nikita Machekhin <nicitamac@gmail.com>
2020-10-26 19:52:56 +0200

    Implements interaction with repository in orders controller

--- src/modules/orders/orders.controller.ts
+++ src/modules/orders/orders.controller.ts
@@ -1,9 +1,62 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Param, Post, Body } from '@nestjs/common';
+import { CreatedOrder, MetaxiError, Order } from './dto';
+import { errors, createError } from '../../helpers/errors';
+import { validate } from 'class-validator';
+import { OrdersRepository } from '../../db/repositories/order.repository';
+import { ClientStatuses } from 'src/db/enums';
 
 @Controller('order')
 export class OrdersController {
+  constructor(private ordersRepository: OrdersRepository) {}
   @Post()
-  createOrder(): string {
-    return 'Order was created';
+  async createOrder(@Body() body): Promise<CreatedOrder | MetaxiError> {
+    const order = new Order(
+      parseInt(body.clientId),
+      parseInt(body.fromId),
+      parseInt(body.toId),
+    );
+    return await validate(order)
+      .then(validationErrors => {
+        if (validationErrors.length) {
+          throw createError(
+            errors.InvalidAPIData,
+            validationErrors
+              .map(error => Object.values(error.constraints))
+              .flat(),
+          );
+        }
+      })
+      .then(() => this.ordersRepository.checkPoint(order.fromId))
+      .then(pointFinded => {
+        if (!pointFinded)
+          throw createError(errors.DepartureNotFound, order.fromId);
+      })
+      .then(() => this.ordersRepository.checkPoint(order.toId))
+      .then(pointFinded => {
+        if (!pointFinded)
+          throw createError(errors.DestinationNotFound, order.toId);
+      })
+      .then(() => this.ordersRepository.checkClient(order.clientId))
+      .then(clientFinded => {
+        if (!clientFinded)
+          throw createError(errors.ClientNotFound, order.clientId);
+        console.log(clientFinded);
+        if (clientFinded.status !== ClientStatuses.Free)
+          throw createError(errors.ClientAlreadyMadeOrder, order.clientId);
+      })
+      .then(() => this.ordersRepository.takeFreeDriver())
+      .then(driver => {
+        if (!driver) {
+          throw createError(errors.NoFreeDrivers);
+        }
+        this.ordersRepository.occupyClient(order.clientId).then(success => {
+          if (!success) {
+            this.ordersRepository.freeDriver(driver.driverId);
+            throw createError(errors.ClientAlreadyMadeOrder, order.clientId);
+          }
+        });
+        return driver;
+      })
+      .catch(error => error);
   }
 }
--- src/modules/orders/orders.module.ts
+++ src/modules/orders/orders.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OrdersController } from './orders.controller';
+import { OrdersRepository } from './../../db/repositories/order.repository';
 
 @Module({
   imports: [],
   controllers: [OrdersController],
-  providers: [],
+  providers: [OrdersRepository],
 })
 export class OrdersModule {}
