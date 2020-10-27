d5e3caae9ae19e6b0d92a5cb028639813ed9d55d
Nikita Machekhin <nicitamac@gmail.com>
2020-10-27 18:53:16 +0200

    Add documentations to the main file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('Metaxi docs')
+    .setDescription('API doc')
+    .setVersion('1.0')
+    .addTag('metaxi')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('', app, document);
   await app.listen(3000);
 }
 bootstrap();
