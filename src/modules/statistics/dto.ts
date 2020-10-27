01059de4b26f6c4736e30bcc3f98a7a02221c73b
Nikita Machekhin <nicitamac@gmail.com>
2020-10-27 18:57:56 +0200

    Make statistics DTO documentable

--- src/modules/statistics/dto.ts
+++ src/modules/statistics/dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsInt, Min } from 'class-validator';
 
 export class DriverId {
@@ -5,8 +6,82 @@ export class DriverId {
   @Min(0)
   driverId: number;
   constructor(driverId: string) {
-    console.log(parseInt(driverId) + 3);
     this.driverId = parseInt(driverId);
-    console.log(this);
+  }
+}
+
+abstract class PostgresInterval {
+  @ApiProperty()
+  milliseconds: number;
+  @ApiProperty()
+  seconds: number;
+  @ApiProperty()
+  minutes: number;
+  @ApiProperty()
+  hours: number;
+}
+
+export class StatisticsCount {
+  @ApiProperty()
+  driverId: number;
+
+  @ApiProperty()
+  ordersCount: number;
+
+  constructor(driverId: string, ordersCount: number) {
+    this.driverId = parseInt(driverId);
+    this.ordersCount = ordersCount;
+  }
+}
+
+export class StatisticsCost {
+  @ApiProperty()
+  driverId: number;
+
+  @ApiProperty()
+  totalCost: number;
+
+  constructor(driverId: string, totalCost: string) {
+    this.driverId = parseInt(driverId);
+    this.totalCost = parseFloat(totalCost);
+  }
+}
+
+export class StatisticsTime {
+  @ApiProperty()
+  driverId: number;
+
+  @ApiProperty()
+  averageRoadTime: PostgresInterval;
+
+  constructor(driverId: string, averageRoadTime: number) {
+    this.driverId = parseInt(driverId);
+    this.averageRoadTime = (averageRoadTime as unknown) as PostgresInterval;
+  }
+}
+
+export class StatisticsFrequentDestination {
+  @ApiProperty()
+  driverId: number;
+
+  @ApiProperty()
+  toPointId: number;
+
+  @ApiProperty()
+  count: number;
+
+  @ApiProperty()
+  pointTitle: string;
+
+  constructor(
+    driverId: string,
+    toPointId: number,
+    count: string,
+    pointTitle: string,
+  ) {
+    this.driverId = parseInt(driverId);
+    this.toPointId = toPointId;
+    this.count = parseInt(count);
+    this.pointTitle = pointTitle;
   }
 }
